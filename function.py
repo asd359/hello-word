f6367cee2cce0f6c5caa1481eddde198f56b565e
asd359 <sjp88incn@live.com>
2016-11-17 20:29:31 +0800

    day2

--- function.py
+++ function.py
@@ -18,6 +18,25 @@ def fun_arg(*arg,**arg1):
     print arg,'  ',type(arg)
     print arg1,' ',type(arg1)
 
+fun_arg('1','trust','belivev',a=9,b='g',sure='yes')
 
+
+print  '全局变量，局部变量'
+a = 10
+def fun5():
+    a = 7
+    print '全局危10，局部为7  .',a
+
+fun5()
+print a
+
+def sum(startInt,endInt):
+    s = 0
+    for i in  range(startInt,endInt+1):
+        s += i
+    return s
+
+
+print '\n\n1 - 100 和为：',sum(1,3)
 
 
